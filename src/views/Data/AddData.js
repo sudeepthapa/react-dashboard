fe4cd38ed3a48ee6b29059eaa581133f6ab868a1
sudeptech <sudeptech@gmail.com>
2019-11-21 22:13:17 +0545

    initial dashboard setup

---
+++ src/views/Data/AddData.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class AddData extends Component {
+  render() {
+    return <div>Add Data Component</div>;
+  }
+}
---
+++ src/views/Data/AllData.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class AllData extends Component {
+  render() {
+    return (
+      <div>
+        All Data Component
+        <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id>
+              First and last name
+            </span>
+          </div>
+          <input type="text" className="form-control" />
+          <input type="text" className="form-control" />
+        </div>
+      </div>
+    );
+  }
+}
